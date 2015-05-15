0db2d05a8d009270d7fe36d60ea45acde0849153
TKEvMkt <tim.kaufeldt@gmail.com>
2015-05-15 09:05:04 -0500

    Refactoring with Ryan

--- dist/js/app.js
+++ dist/js/app.js
@@ -38,27 +38,49 @@ app.controller('TaskLoader', [
 		var MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
 		var MILLISECONDS_PER_WEEK = 7 * MILLISECONDS_PER_DAY;
 
+		// var updateObject = function(original, updates) {
+
+		// };
+
+		// var priorityChoices = {
+		// 	0: "High",
+		// 	1: "Medium",
+		// 	2: "Low",
+		// };
+		// $scope.priorityChoices = priorityChoices;
+
+		// var getPriorityDisplayValue = function(id) {
+		// 	return priorityChoices[id];
+		// };
+
+		// $scope.getPriorityDisplayValue = getPriorityDisplayValue;
 		
-		
-      	$scope.master = {};
+      	// $scope.master = {};
 
 	      $scope.update = function(task) {
-	        $scope.master = angular.copy(task);
-	        var taskString = $scope.master.task;
-			var priorityString = $scope.master.priority;
 			var createdAt = (new Date().getTime());
 			var destructAt = (createdAt+MILLISECONDS_PER_WEEK);
 			var destructDate = new Date(destructAt);
 			var destructEd = (destructDate.toString());
-			
-				var newTask = {
-					task: taskString,
-					priority: priorityString,
-					created: createdAt,
-					destruct: destructAt,
-					killtime: destructEd
-				};
 
+			var now = (new Date()).getTime();
+			var newTask = angular.copy(task);
+			var newTaskDefaultDates = {
+				created: now,
+				destruct: destructAt,
+				killtime: destructEd
+			};
+			newTask.created = newTaskDefaultDates.created;
+			newTask.destruct = newTaskDefaultDates.destruct;
+			newTask.killtime = newTaskDefaultDates.killtime;
+
+			// // newTask.update(newTaskDefaultDates);
+			// for (prop in newTaskDefaultDates) {
+			// 	if (newTaskDefaultDates.hasOwnProperty(prop)) {
+			// 		newTask[prop] = newTaskDefaultDates[prop];
+			// 	}
+			// }
+			// updateObject(newTask, newTaskDefaultDates)
 
 
 			$scope.fbTaskTesting.$add(newTask);
